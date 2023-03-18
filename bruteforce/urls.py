651dab35b5d1fa818876f3ca2222b463499a1fbf
roadluck <roadluck@localhost.localdomain>
2023-03-18 01:03:47 -0300

    ignore and path

--- bruteforce/urls.py
+++ bruteforce/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BruteForceLoginView, DashboardView, attack_stats
 
 urlpatterns = [
-    path('admin/login/', BruteForceLoginView.as_view(), name='login'),
+    path('wp-login.php', BruteForceLoginView.as_view(), name='login'),
      path('admin/attack_stats/', attack_stats, name='attack_stats'),
     path('', DashboardView.as_view(), name='index'),
 ]
